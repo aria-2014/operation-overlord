57296fc85227efc9e3b742340a13752f40b18f3c
mheduan <mheduan@gmail.com>
2014-04-16 16:12:18 +0100

    Map + basis dnd branch martin

--- ARIAHTM/app/Scripts/ARIA001.js
+++ ARIAHTM/app/Scripts/ARIA001.js
@@ -44,3 +44,38 @@
 		});
 
 });
+
+
+
+
+
+
+
+
+
+// Create a div to hold the control.
+var controlDiv = document.createElement('div');
+
+// Set CSS styles for the DIV containing the control
+// Setting padding to 5 px will offset the control
+// from the edge of the map.
+controlDiv.style.padding = '5px';
+
+// Set CSS for the control border.
+var controlUI = document.createElement('div');
+controlUI.style.backgroundColor = 'white';
+controlUI.style.borderStyle = 'solid';
+controlUI.style.borderWidth = '2px';
+controlUI.style.cursor = 'pointer';
+controlUI.style.textAlign = 'center';
+controlUI.title = 'Click to set the map to Home';
+controlDiv.appendChild(controlUI);
+
+// Set CSS for the control interior.
+var controlText = document.createElement('div');
+controlText.style.fontFamily = 'Arial,sans-serif';
+controlText.style.fontSize = '12px';
+controlText.style.paddingLeft = '4px';
+controlText.style.paddingRight = '4px';
+controlText.innerHTML = '<strong>Home</strong>';
+controlUI.appendChild(controlText);
--- ARIAHTM/app/Scripts/ARIA003.js
+++ ARIAHTM/app/Scripts/ARIA003.js
@@ -1,5 +1,52 @@
 ﻿$(document).ready(function () {
 
+    google.maps.event.addListener(map, "rightclick", function (event) { showContextMenu(event.latLng); });
+    function showContextMenu(caurrentLatLng) {
+        var projection;
+        var contextmenuDir;
+        projection = map.getProjection();
+        $('.contextmenu').remove();
+        contextmenuDir = document.createElement("div");
+        contextmenuDir.className = 'contextmenu';
+        contextmenuDir.innerHTML = '<a id="menu1"><div class="context">menu item 1<\/div><\/a>'
+                                + '<a id="menu2"><div class="context">menu item 2<\/div><\/a>';
 
+        $(map.getDiv()).append(contextmenuDir);
+
+        setMenuXY(caurrentLatLng);
+
+        contextmenuDir.style.visibility = "visible";
+    }
+    function getCanvasXY(caurrentLatLng) {
+        var scale = Math.pow(2, map.getZoom());
+        var nw = new google.maps.LatLng(
+            map.getBounds().getNorthEast().lat(),
+            map.getBounds().getSouthWest().lng()
+        );
+        var worldCoordinateNW = map.getProjection().fromLatLngToPoint(nw);
+        var worldCoordinate = map.getProjection().fromLatLngToPoint(caurrentLatLng);
+        var caurrentLatLngOffset = new google.maps.Point(
+            Math.floor((worldCoordinate.x - worldCoordinateNW.x) * scale),
+            Math.floor((worldCoordinate.y - worldCoordinateNW.y) * scale)
+        );
+        return caurrentLatLngOffset;
+    }
+      function setMenuXY(caurrentLatLng){
+     var mapWidth = $('#map_canvas').width();
+     var mapHeight = $('#map_canvas').height();
+     var menuWidth = $('.contextmenu').width();
+     var menuHeight = $('.contextmenu').height();
+     var clickedPosition = getCanvasXY(caurrentLatLng);
+     var x = clickedPosition.x ;
+     var y = clickedPosition.y ;
+
+      if((mapWidth - x ) < menuWidth)//if to close to the map border, decrease x position
+          x = x - menuWidth;
+     if((mapHeight - y ) < menuHeight)//if to close to the map border, decrease y position
+         y = y - menuHeight;
+
+     $('.contextmenu').css('left',x  );
+     $('.contextmenu').css('top',y );
+     };
 
 });
