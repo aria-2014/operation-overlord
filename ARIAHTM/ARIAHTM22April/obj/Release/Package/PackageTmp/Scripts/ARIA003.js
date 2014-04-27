a8a2fe851444c024274abe0aaacdbbbefbd001e8
Brian <fredyfontaine@yahoo.co.uk>
2014-04-28 00:16:47 +0100

    CSS - Accordions for menu & graph Put 'layers' section (under search section) & Martin's weather graph on Map page into collapsible / expandable Bootstrap accordions & add CSS to search buttons

---
+++ ARIAHTM/ARIAHTM22April/obj/Release/Package/PackageTmp/Scripts/ARIA003.js
@@ -0,0 +1,23 @@
+﻿function myPinterestBoardsController($scope) {
+    var pins = [];
+    $.ajax({
+        //url: "https://ismaelc-pinterest.p.mashape.com/TourismIreland/pins",
+        url: "https://ismaelc-pinterest.p.mashape.com/TourismIreland/boards",
+        headers: { 'X-Mashape-Authorization': 'L6Abj40efQ4Q4G5pDOml48z0UP8ZEQPw' },
+        dataType: "json",
+        async: false,
+        success: function (data) {
+            $.each(data, function (index, value) {
+                if (index == "body") {
+                    $.each(value, function (index1, value1) {
+                        pins.push({ pinboardslink: "http://pinterest.com/" + value1.href, pinboardspic: value1.src, pinboardsdesc: value1.name });
+                    });
+                }
+            });
+        },
+        error: function (data) {
+            alert("error");
+        }
+    });
+    $scope.services = pins;
+}
