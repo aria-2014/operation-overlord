909f7c7a8a5021be28eb0ca1137fbb84aec1a3c4
Richard Nevin <richard.nevin@outlook.com>
2014-05-02 01:30:26 +0100

    Dummy tests

---
+++ ARIAHTM/ARIAHTM22April/Scripts/TestDummy.js
@@ -0,0 +1,10 @@
+var TestDummy = (function() {
+
+    return {
+        TestDummy1: function(x) {
+            var y = x + 1;
+            return y;
+        }
+
+    }
+}())
---
+++ ARIAHTM/test/spec/TestDummySpec.js
@@ -0,0 +1,15 @@
+describe("My Test Dummy", function() {
+	
+	it("TestDummy1: Should Pass ", function() {
+	
+        expect(TestDummy.TestDummy1(1)).toBe(2);
+		
+    });
+
+    it("TestDummy1: Should Pass", function() {
+	
+        expect(TestDummy.TestDummy1(2)).toBe(3);
+		
+    });
+	
+})
